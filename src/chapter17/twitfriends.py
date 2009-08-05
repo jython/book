96e7dd3a5566397e38c6b1a59e3f130d20de31ec
Frank Wierzbicki <fwierzbicki@gmail.com>
2009-08-05 16:16:36 -0400

    Cleanup, better default size.

--- src/chapter17/twitfriends.py
+++ src/chapter17/twitfriends.py
@@ -2,9 +2,10 @@
 import twitter
 import re
 
-from javax.swing import *
-from java.lang import *
-from java.awt import *
+from javax.swing import (BoxLayout, ImageIcon, JButton, JFrame, JPanel,
+        JPasswordField, JLabel, JTextArea, JTextField, JScrollPane,
+        SwingConstants, WindowConstants)
+from java.awt import Component, GridLayout
 from java.net import URL
 from java.lang import Runnable
 
@@ -39,14 +40,13 @@ class JyTwitter(object):
         username = self.usernameField.text
         try:
             self.api = twitter.Api(username, self.passwordField.text)
-            self.frame.size = 300, 400
             self.timeline(username)
             self.loginPanel.visible = False
             self.message.text = "Logged in"
         except:
             self.message.text = "Log in failed."
             raise
-        self.frame.size = 300, 400
+        self.frame.size = 400,800
         self.frame.show()
 
     def timeline(self, username):
@@ -54,27 +54,30 @@ class JyTwitter(object):
         self.resultPanel = JPanel()
         self.resultPanel.layout = BoxLayout(self.resultPanel, BoxLayout.Y_AXIS)
         for s in timeline:
-            self.makeLabel(s)
+            self.showTweet(s)
 
         scrollpane = JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                                  JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
-        scrollpane.preferredSize = 300, 300
+        scrollpane.preferredSize = 400, 800
         scrollpane.viewport.view = self.resultPanel
 
         self.frame.add(scrollpane)
 
-    def makeLabel(self, status):
+    def showTweet(self, status):
         user = status.user
         p = JPanel()
+
+        # image grabbing seems very expensive, good place for a callback?
         p.add(JLabel(ImageIcon(URL(user.profile_image_url))))
+
         p.add(JTextArea(text = status.text,
                         editable = False,
-                        alignmentX = Component.LEFT_ALIGNMENT,
+                        wrapStyleWord = True,
                         lineWrap = True,
-                        size = (300, 30)
+                        alignmentX = Component.LEFT_ALIGNMENT,
+                        size = (300, 1)
              ))
         self.resultPanel.add(p)
-        print user.profile_image_url
 
 if __name__ == '__main__':
     JyTwitter()
